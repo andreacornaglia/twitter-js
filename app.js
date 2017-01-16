ffabebdd5cc5d649f23c6bbf483891552c8c5d3c
Aly Khayrulina <a.a.khayrulina@gmail.com>
2017-01-16 18:25:16 -0500

    update app with middleware

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.use("/special/",function(req,res,next){
+  console.log("This is a special place.");
+  next();
+})
+
+app.use(function(req,res,next){
+  console.log("Request: ",req.method,req.path);
+  next();
+})
+
 app.get("/",function(req,res,next){
   res.send("Welcome!!")
 })
