15e4ad569994b04090fd5aef22acd298143b7273
Andrea Cornaglia <andreacornaglia@gmail.com>
2017-01-17 10:52:24 -0500

    template linked to express

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const app = express();
+
+//connecting express w/nunjuncks
+app.set("view engine", "html");
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true }); //cache off during development
 
 app.use("/special/",function(req,res,next){
   console.log("This is a special place.");
@@ -13,7 +19,9 @@ app.use(function(req,res,next){
 })
 
 app.get("/",function(req,res,next){
-  res.send("Welcome!!")
+  //res.send("Welcome!!");
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
 app.get("/news",function(req,res,next){
@@ -23,3 +31,19 @@ app.get("/news",function(req,res,next){
 app.listen(3000, function(){
   console.log("Server listening")
 })
+
+//nunjuncks code goes here
+/*
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.render('index.html', locals, function(err, output) {
+  console.log(output);
+});
+*/
