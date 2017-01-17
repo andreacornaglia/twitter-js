b2e29501e71e5cd75876303ce2fe70a246baf605
Andrea Cornaglia <andreacornaglia@gmail.com>
2017-01-17 12:16:23 -0500

    created routs and updated css and index files

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
-
+const routes = require('./routes/');
 const app = express();
 
 //connecting express w/nunjuncks
 app.set("view engine", "html");
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', { noCache: true }); //cache off during development
+
+app.use('/', routes);
 
 app.use("/special/",function(req,res,next){
   console.log("This is a special place.");
@@ -18,32 +20,6 @@ app.use(function(req,res,next){
   next();
 })
 
-app.get("/",function(req,res,next){
-  //res.send("Welcome!!");
-  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
-
-app.get("/news",function(req,res,next){
-  res.send("This is the news page.")
-})
-
 app.listen(3000, function(){
   console.log("Server listening")
 })
-
-//nunjuncks code goes here
-/*
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-
-nunjucks.render('index.html', locals, function(err, output) {
-  console.log(output);
-});
-*/
